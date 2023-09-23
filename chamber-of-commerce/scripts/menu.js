edb561218286d47d0953470a37526e1b4fe742c9
Andrew Blad <114179593+andrewdblad@users.noreply.github.com>
2023-09-23 08:33:45 -0600

    chamber of commerce home

---
+++ chamber-of-commerce/scripts/menu.js
@@ -0,0 +1,7 @@
+// JavaScript code for handling the mobile hamburger menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
